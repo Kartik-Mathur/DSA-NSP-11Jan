9633138287a46f4daec053b580e96f53dc698c35
Kartik <kartik@codingblocks.com>
2026-03-08 15:36:44 +0530

    Recursion

--- Lecture-5-Arrays/6_BubbleSort.cpp
+++ Lecture-5-Arrays/6_BubbleSort.cpp
@@ -6,7 +6,7 @@ int main() {
 	int a[] = {5, 4, 3, 2, 1};
 	int n = sizeof(a) / sizeof(int);
 
-	for (int i = 0; i < n - 1; ++i) // To place n-1 elements on its correct position
+	for (int i = 0; i < n - 1; ++i)
 	{
 		for (int j = 0; j < n - 1 - i; ++j)
 		{
